af99158e3b9222658d34333083b195890dc4f795
= <=>
2020-01-26 10:21:05 +0200

    =

--- resources/views/admin/includes/alerts/errors.blade.php
+++ resources/views/admin/includes/alerts/errors.blade.php
@@ -1,9 +1,7 @@
 @if(Session::has('error'))
-    <div class="row">
-        <div class="col-md-6  offset-3">
+    <div class="row mr-2 mr-2" >
             <button type="text" class="btn btn-lg btn-block btn-outline-danger mb-2"
                     id="type-error">{{Session::get('error')}}
             </button>
-        </div>
     </div>
 @endif
--- resources/views/admin/includes/alerts/success.blade.php
+++ resources/views/admin/includes/alerts/success.blade.php
@@ -1,9 +1,7 @@
 @if(Session::has('success'))
-    <div class="row">
-        <div class="col-md-6  offset-3">
+    <div class="row mr-2 ml-2">
             <button type="text" class="btn btn-lg btn-block btn-outline-success mb-2"
                     id="type-error">{{Session::get('success')}}
             </button>
-        </div>
     </div>
 @endif
