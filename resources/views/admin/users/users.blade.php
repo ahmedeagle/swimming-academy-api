d32925d2762c0d30b0f8e77060f392158d273a43
= <=>
2020-02-22 17:14:47 +0200

    =

--- resources/views/admin/users/users.blade.php
+++ resources/views/admin/users/users.blade.php
@@ -1,6 +1,7 @@
 @section('main')
     @if(isset($users) && $users -> count() > 0 )
         @foreach($users as $user)
+            <tr>
             <td>{{$user -> name_ar}}</td>
             <td><img src="{{$user -> photo}}" height="40px;"></td>
             <td>{{$user -> academy -> name_ar}}</td>
@@ -18,6 +19,7 @@
                     />
                 </div>
             </td>
+            </tr>
         @endforeach
     @endif
 @stop
