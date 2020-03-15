170cb2c3c646f28cf0480577385da352445a4006
= <=>
2020-03-15 22:28:00 +0200

    =

--- app/Traits/TeamTrait.php
+++ app/Traits/TeamTrait.php
@@ -17,7 +17,6 @@ trait TeamTrait
     {
         return Team::find($teamId)
             -> users()
-            ->where('users.acasemy')
             ->with(['team' => function ($city) {
             $city->select('id',DB::raw('name_' . app()->getLocale() . ' as name'),DB::raw('level_' . app()->getLocale() . ' as level'));
         }])
